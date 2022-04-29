31e713ba4e23b9e975daf59401f30b62e6e89265
Eul Rho <qkekshdmf777@gmail.com>
2022-04-29 09:27:40 +0900

    (Add)model table

--- location/models.py
+++ location/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-
+class ParkingLot(models.Model):
+    name = models.CharField(max_length=200)
+    latitude = models.FloatField(default=0.0)
+    longitude = models.FloatField(default=0.0)
